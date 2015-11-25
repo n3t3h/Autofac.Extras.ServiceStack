8f0bcd791e2b70093b10584cc429b3cf26dd83df
Alexey Zimarev <alzi@abax.no>
2015-11-25 14:39:59 +0100

    Initial commit

---
+++ Autofac.Extras.ServiceStack/AutofacIocAdapter.cs
@@ -0,0 +1,28 @@
+﻿using ServiceStack.Common;
+using ServiceStack.Configuration;
+
+namespace Autofac.Extras.ServiceStack
+{
+    public class AutofacIocAdapter : IContainerAdapter
+    {
+        public AutofacIocAdapter(IContainer container)
+        {
+            Container = container;
+        }
+
+        public IContainer Container { get; }
+
+        public T Resolve<T>()
+        {
+            return !HostContext.Instance.Items.Contains("AutofacScope")
+                ? Container.Resolve<T>()
+                : ((ILifetimeScope) HostContext.Instance.Items["AutofacScope"]).Resolve<T>();
+        }
+
+        public T TryResolve<T>()
+        {
+            T result;
+            return Container.TryResolve(out result) ? result : default(T);
+        }
+    }
+}
---
+++ Autofac.Extras.ServiceStack/AutofacLifecycleFilter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Autofac.Core.Lifetime;
+using ServiceStack.Common;
+using ServiceStack.WebHost.Endpoints;
+
+namespace Autofac.Extras.ServiceStack
+{
+    public static class ServiceStackAutofac
+    {
+        public static void UseAutofac(this AppHostBase appHost, IContainer container)
+        {
+            appHost.Container.Adapter = new AutofacIocAdapter(container);
+            appHost.RequestFilters.Add((req, resp, dto) =>
+            {
+                var scope = container.BeginLifetimeScope(MatchingScopeLifetimeTags.RequestLifetimeScopeTag);
+                HostContext.Instance.Items["AutofacScope"] = scope;
+            });
+            appHost.ResponseFilters.Add((req, resp, dto) =>
+            {
+                var scope = HostContext.Instance.Items["AutofacScope"] as IDisposable;
+                scope?.Dispose();
+            });
+        }
+    }
+}
