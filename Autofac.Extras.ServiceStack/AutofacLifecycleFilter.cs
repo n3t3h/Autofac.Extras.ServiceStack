8e81fb4309cebdd7154fd81b90c2423bb155f8a7
Alexey Zimarev <alzi@abax.no>
2015-11-25 15:11:36 +0100

    Adding both types of hosts

--- Autofac.Extras.ServiceStack/AutofacLifecycleFilter.cs
+++ Autofac.Extras.ServiceStack/AutofacLifecycleFilter.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using Autofac.Core.Lifetime;
+using Funq;
 using ServiceStack.Common;
 using ServiceStack.WebHost.Endpoints;
+using ServiceStack.WebHost.Endpoints.Support;
 
 namespace Autofac.Extras.ServiceStack
 {
@@ -9,7 +11,23 @@ namespace Autofac.Extras.ServiceStack
     {
         public static void UseAutofac(this AppHostBase appHost, IContainer container)
         {
+            appHost.AddAutofacAdapter(container);
+            appHost.AddPerRequestScope(container);
+        }
+
+        public static void UseAutofac(this HttpListenerBase appHost, IContainer container)
+        {
+            appHost.AddAutofacAdapter(container);
+            appHost.AddPerRequestScope(container);
+        }
+
+        public static void AddAutofacAdapter(this IHasContainer appHost, IContainer container)
+        {
             appHost.Container.Adapter = new AutofacIocAdapter(container);
+        }
+
+        public static void AddPerRequestScope(this IAppHost appHost, IContainer container)
+        {
             appHost.RequestFilters.Add((req, resp, dto) =>
             {
                 var scope = container.BeginLifetimeScope(MatchingScopeLifetimeTags.RequestLifetimeScopeTag);
